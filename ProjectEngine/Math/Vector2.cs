e9522b650d963cc5eb877ee04ce9917fee67bd4c
stroywzh <1431311795@qq.com>
2026-08-11 18:31:49 +0800

    feat: add Vector2 math type and MouseState with position delta, button, scroll

---
+++ ProjectEngine/Math/Vector2.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ProjectEngine.Math;
+
+public struct Vector2 : IEquatable<Vector2>
+{
+    public float X, Y;
+
+    public Vector2(float x, float y) { X = x; Y = y; }
+
+    public static readonly Vector2 Zero = new(0, 0);
+    public static readonly Vector2 One = new(1, 1);
+
+    public static Vector2 operator -(Vector2 a, Vector2 b) => new(a.X - b.X, a.Y - b.Y);
+
+    public static bool operator ==(Vector2 a, Vector2 b) => a.X == b.X && a.Y == b.Y;
+    public static bool operator !=(Vector2 a, Vector2 b) => !(a == b);
+
+    public bool Equals(Vector2 other) => this == other;
+    public override bool Equals(object? obj) => obj is Vector2 v && Equals(v);
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+    public override string ToString() => $"({X}, {Y})";
+}
---
+++ tests/ProjectEngine.Tests/Input/MouseStateTests.cs
@@ -0,0 +1,52 @@
+using ProjectEngine;
+using ProjectEngine.Math;
+
+namespace ProjectEngine.Tests.Input;
+
+public class MouseStateTests
+{
+    [Fact]
+    public void SetPosition_UpdatesPosition()
+    {
+        var ms = new MouseState();
+        ms.SetPosition(new Vector2(100, 200));
+        Assert.Equal(new Vector2(100, 200), ms.Position);
+    }
+
+    [Fact]
+    public void MoveVector_IsDeltaSinceLastSwap()
+    {
+        var ms = new MouseState();
+        ms.SetPosition(new Vector2(100, 100));
+        ms.SwapBuffers();
+        ms.SetPosition(new Vector2(105, 103));
+        Assert.Equal(new Vector2(5, 3), ms.MoveVector);
+    }
+
+    [Fact]
+    public void LeftButton_AfterSet_ReturnsTrue()
+    {
+        var ms = new MouseState();
+        ms.SetButton(0, true);
+        Assert.True(ms.LeftButton);
+    }
+
+    [Fact]
+    public void GetButtonDown_FirstFrame_ReturnsTrue()
+    {
+        var ms = new MouseState();
+        ms.SwapBuffers();
+        ms.SetButton(1, true);
+        Assert.True(ms.GetButtonDown(1));
+    }
+
+    [Fact]
+    public void ScrollDelta_ResetsAfterSwap()
+    {
+        var ms = new MouseState();
+        ms.ScrollDelta = 1.2f;
+        Assert.Equal(1.2f, ms.ScrollDelta);
+        ms.SwapBuffers();
+        Assert.Equal(0f, ms.ScrollDelta);
+    }
+}
