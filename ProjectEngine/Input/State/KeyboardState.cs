9afe2589a87494b8ec10910ca8a6705222291847
stroywzh <1431311795@qq.com>
2026-08-11 18:30:15 +0800

    feat: add KeyboardState with prev/curr double-buffer and GetKeyDown/GetKeyUp

--- ProjectEngine/Input/State/KeyboardState.cs
+++ ProjectEngine/Input/State/KeyboardState.cs
@@ -1,3 +1,17 @@
+using System.Collections.Generic;
+using ProjectEngine.Input;
+
 namespace ProjectEngine;
 
-public class KeyboardState { }
+public class KeyboardState
+{
+    private HashSet<KeyCode> _prev = new(), _curr = new();
+
+    public bool GetKey(KeyCode key) => _curr.Contains(key);
+    public bool GetKeyDown(KeyCode key) => !_prev.Contains(key) && _curr.Contains(key);
+    public bool GetKeyUp(KeyCode key) => _prev.Contains(key) && !_curr.Contains(key);
+    public bool AnyKey => _curr.Count > 0;
+
+    public void SwapBuffers() { _prev = _curr; _curr = new HashSet<KeyCode>(); }
+    public void SetKey(KeyCode key, bool pressed) { if (pressed) _curr.Add(key); }
+}
---
+++ tests/ProjectEngine.Tests/Input/KeyboardStateTests.cs
@@ -0,0 +1,54 @@
+using ProjectEngine;
+using ProjectEngine.Input;
+
+namespace ProjectEngine.Tests.Input;
+
+public class KeyboardStateTests
+{
+    [Fact]
+    public void GetKey_AfterSetKey_ReturnsTrue()
+    {
+        var kb = new KeyboardState();
+        kb.SetKey(KeyCode.A, true);
+        Assert.True(kb.GetKey(KeyCode.A));
+    }
+
+    [Fact]
+    public void GetKeyDown_FirstFrame_ReturnsTrue()
+    {
+        var kb = new KeyboardState();
+        kb.SwapBuffers();                       // 帧边界：prev为空
+        kb.SetKey(KeyCode.Space, true);         // 本帧首次按下
+        Assert.True(kb.GetKeyDown(KeyCode.Space));
+    }
+
+    [Fact]
+    public void GetKeyDown_HeldSecondFrame_ReturnsFalse()
+    {
+        var kb = new KeyboardState();
+        kb.SetKey(KeyCode.W, true);
+        kb.SwapBuffers();
+        kb.SetKey(KeyCode.W, true);
+        kb.SwapBuffers();
+        kb.SetKey(KeyCode.W, true);
+        Assert.False(kb.GetKeyDown(KeyCode.W));
+    }
+
+    [Fact]
+    public void GetKeyUp_OnRelease_ReturnsTrue()
+    {
+        var kb = new KeyboardState();
+        kb.SetKey(KeyCode.D, true);
+        kb.SwapBuffers();
+        kb.SetKey(KeyCode.D, false);
+        Assert.True(kb.GetKeyUp(KeyCode.D));
+    }
+
+    [Fact]
+    public void AnyKey_WhenPressed_ReturnsTrue()
+    {
+        var kb = new KeyboardState();
+        kb.SetKey(KeyCode.A, true);
+        Assert.True(kb.AnyKey);
+    }
+}
