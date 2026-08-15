0de0626dde034f5e8e81c2cebae01a0a5bb4ec75
stroywzh <1431311795@qq.com>
2026-08-15 14:08:37 +0800

    1

--- src/Sandbox/Program.cs
+++ src/Sandbox/Program.cs
@@ -11,16 +11,17 @@ class Program
         var backend = new OpenGLRenderBackend();
         var engine = new EngineLoop(backend);
         Input.EnableLog = true;
+        LogConfig.Render = false;
 
         // -------------------- 逐个取消注释测试 --------------------
         // Demos.TestSingleCube.Run(engine);
         // Demos.TestNDCTriangle.Run(engine);
         // Demos.TestNDCQuad.Run(engine);
         // Demos.TestCameraOrtho.Run(engine);
-        // Demos.TestCameraPerspective.Run(engine);
+        Demos.TestCameraPerspective.Run(engine);
         // Demos.TestPNGQuad.Run(engine);
         // Demos.TestThirdPerson3D.Run(engine);
-        Demos.TestPNGQuad.Run(engine);
+        // Demos.TestPNGQuad.Run(engine);
         // ---------------------------------------------------------
 
         engine.Initialize().Run();
